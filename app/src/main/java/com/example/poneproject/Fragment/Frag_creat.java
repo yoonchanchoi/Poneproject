64ee5d3e0d87c240056c2f2d5daca087d88c7d8d
yoonchanchoi <choyc9239@gmail.com>
2022-01-13 00:41:17 +0900

    test commit

---
+++ app/src/main/java/com/example/poneproject/Fragment/Frag_creat.java
@@ -0,0 +1,22 @@
+package com.example.poneproject.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.poneproject.R;
+
+
+public class Frag_creat extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag_creat, container, false);
+    }
+}
